62447d31103b84718055c07ee71b6d0bbd2813f1
Becky <bjentwistle@gmail.com>
2023-05-25 13:52:08 +0100

    end of lesson

--- src/components/LaunchSelector.js
+++ src/components/LaunchSelector.js
@@ -1,10 +1,31 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  font-size: 1em;
+  padding: 1em;
+  margin: 0.5em;
+  border: 4px solid #ced7e0;
+  background: ${(props) => props.background || "#9ccddc"}; 
+`
+
+const ButtonContainer = styled.div`
+  display:flex; 
+  justify-content:center;
+  background-color: #042f4f;
+`
+
+// or statement to give the buttons different colours
+
 const LaunchSelector = ({launchIncrease, launchDecrease}) => {
 
   return (
     <>
-      <button onClick={launchDecrease}>Previous Launch</button>
-      <button onClick={launchIncrease}>Next Launch</button>
-    </>
+    <ButtonContainer>
+      <Button onClick={launchDecrease}>Previous Launch</Button>
+      <Button onClick={launchIncrease} background = "#dcab9c" >Next Launch</Button>
+    </ButtonContainer>
+      </>
+    
   )
 
 }
